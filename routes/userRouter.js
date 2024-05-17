bd9aadace1a79b65cf9672e2f7df4a061ab0c949
HSQMG <9981hoang@gmail.com>
2024-05-17 16:58:39 +0700

    update route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const controller = require("../controllers/userController");
 
 router.get("/", controller.show);
+router.post("/", controller.addUser);
+router.put("/", controller.editUser);
+router.delete("/:id", controller.deleteUser);
 
 module.exports = router;
